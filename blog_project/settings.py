ba22d8d3fec30ca9391046d2bfc81c64274a8e4b
angelacpd <angelacrepaldi@gmail.com>
2021-08-22 21:10:05 -0300

    Install django-allauth and add settings for user registration.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -47,11 +47,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # 3rd-party apps
     'rest_framework',
     'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'rest_auth',
+    'rest_auth.registration',
 
     # Local
     'posts.apps.PostsConfig',
@@ -151,3 +156,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
