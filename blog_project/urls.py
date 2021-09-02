c5cc6d4b22727cac8f617e0b05979415afa72ccc
angelacpd <angelacrepaldi@gmail.com>
2021-09-01 22:54:50 -0300

    Restrict access to swagger.

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -29,15 +29,15 @@ API_DESCRIPTION = 'A Web API for creating and editing blog posts.'
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="API_TITLE",
       default_version='v1',
-      description="Test description",
+      description=API_DESCRIPTION,
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
-   permission_classes=[permissions.AllowAny],
+   permission_classes=[permissions.IsAuthenticated],
 )
 
 
@@ -51,9 +51,8 @@ urlpatterns = [
         title=API_TITLE,
         description=API_DESCRIPTION
     )),
-    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # path('schema/', schema_view),
-    # path('swagger-docs/', schema_view),
 ]
